fd5537687f9745f010f3617f344ae667fc8206c2
NickyEXE <dovern42@gmail.com>
2020-07-21 16:10:36 -0400

    crud on superhero

--- app/controllers/superheros_controller.rb
+++ app/controllers/superheros_controller.rb
@@ -1,2 +1,51 @@
 class SuperherosController < ApplicationController
+  before_action :set_superhero, only: [:edit, :update, :show, :destroy]
+
+  def show
+  end
+
+  def index
+    @superheros = Superhero.all
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.new(superhero_params)
+    if @superhero.save
+      redirect_to @superhero
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @superhero.update(superhero_params)
+      redirect_to @superhero
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @superhero.destroy
+    redirect_to :superheros_path
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :alias, :power, :power_level)
+  end
+
+  def set_superhero
+    @superhero = Superhero.find(params[:id])
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_194418) do
+ActiveRecord::Schema.define(version: 2020_07_21_194456) do
 
   create_table "superheros", force: :cascade do |t|
     t.string "name"
