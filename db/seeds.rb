fbd77fea28e4e3d7c59df7fa9f7196614205ba3d
NickyEXE <dovern42@gmail.com>
2020-07-21 16:36:06 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Superhero.create(name: "America Chavez", alias: "Miss America", power: "Punching through dimensions", power_level: 7)
+Superhero.create(name: "Doreen Green", alias: "Squirrel Girl", power: "Eating Nuts and Kicking Butts", power_level: 10)
+Superhero.create(name: "Invisible Woman", alias: "Sue Storm", power: "Invisibility, energy fields", power_level: 7)
+Superhero.create(name: "Loki", alias: "Loki", power: "Tricksiness", power_level: 8)
+Superhero.create(name: "Billy Kaplan", alias: "Wiccan", power: "Can literally just change the universe with a thought.", power_level: 10)
+Superhero.create(name: "Dorrek VIII", alias: "Hulkling", power: "Shapeshifter, not being the hulk but being mistaken for him because he's green.", power_level: 4)
+Superhero.create(name: "Miles Morales", alias: "Spider-Man", power: "Webs, Proportionate Speed and Strength of a Spider, Camoflauge", power_level: 5)
